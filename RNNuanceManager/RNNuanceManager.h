dcf0def73a69a53f539bc525aa466341377cd0fc
Peter Banka <pbanka@ciena.com>
2016-03-26 12:09:48 -0700

    Initial Commit

---
+++ RNNuanceManager/RNNuanceManager.h
@@ -0,0 +1,13 @@
+//
+//  RNNuanceManager.h
+//  RNNuanceManager
+//
+//  Created by Peter Banka on 3/26/16.
+//  Copyright © 2016 Peter Banka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNNuanceManager : NSObject
+
+@end
---
+++ RNNuanceManager/RNNuanceManager.m
@@ -0,0 +1,13 @@
+//
+//  RNNuanceManager.m
+//  RNNuanceManager
+//
+//  Created by Peter Banka on 3/26/16.
+//  Copyright © 2016 Peter Banka. All rights reserved.
+//
+
+#import "RNNuanceManager.h"
+
+@implementation RNNuanceManager
+
+@end
